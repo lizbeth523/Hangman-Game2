fc56b6aa967891f44856bbb7ce7d2deba1998ad6
Elizabeth Tom <elizabeth_tom@excite.com>
2017-10-19 14:14:01 -0500

    Update 10/19/17 14:13

--- Letter.js
+++ Letter.js
@@ -1,16 +1,12 @@
 var Letter = function(value) {
 	this.value = value;
-	this.hidden = true;
 
-	// Display a blank space for any whitespace character
 	if (this.value.match(/\s/)) {
 		this.display = " ";
 	}
-	// If the letter has not yet been guessed, display an underscore
-	else if (this.value.match(/[A-Za-z]/) && this.hidden) {
+	else if (this.value.match(/^[A-Za-z]/)) {
 		this.display = "_";
 	}
-	// If the letter has been guessed or if the character is not a letter, display the value
 	else {
 		this.display = value;
 	}
--- hangman.js
+++ hangman.js
@@ -5,8 +5,7 @@ var wordObject = new Word();
 var callback = function(word, definition, example) {
  	console.log("word: " + word);
  	console.log("definition: " + definition);
- 	console.log("example: " + example);
- 	console.log("callback fxn is running");
+ 	console.log("example: " + example + "\n");
 
  	var letters = createLetters(word);
  	playGame(letters);
@@ -26,13 +25,53 @@ var callback = function(word, definition, example) {
 
  var playGame = function(letters) {
  	var inquirer = require('inquirer');
+
  	
-	inquirer.prompt([/* Pass your questions in here */
+	inquirer.prompt([
+	{
+		type: "input",
+		message: "Guess a letter! ",
+		name: "guess",
+		validate: function(value) {
+			if (value.match(/[A-Za-z]/)) {
+				return true;
+			}
+			else {
+				return "Please guess a letter";
+			}
+		}
+	}
 	]).then(function (answers) {
-    // Use user feedback for... whatever!!
+    	// answers.guess = answers.guess.toUpperCase();
+    	var containsGuess = false;
+    	for (var i = 0; i < letters.length; i++) {
+    		if (letters[i].value === answers.guess.toUpperCase()) {
+    			letters[i].display = letters[i].value;
+    			containsGuess = true;
+    		}
+    	}
+    	
+    	updateDisplay(containsGuess, letters);
 	});
+ 	
  }
 
+
+ var updateDisplay = function(containsGuess, letters) {
+ 	var displayString = "";
+
+ 	if (containsGuess) {
+ 		console.log("CORRECT!");
+ 	}
+ 	else {
+ 		console.log("INCORRECT :'\(");
+ 	}
+
+ 	for (i in letters) {
+ 		displayString += letters[i].display + " ";
+ 	}
+ 	console.log(displayString);
+ }
 
  wordObject.getWord(callback);
 
