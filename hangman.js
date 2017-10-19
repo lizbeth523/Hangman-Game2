1fbf3612a9deb3e235baba479e754069118d814c
Elizabeth Tom <elizabeth_tom@excite.com>
2017-10-19 16:05:52 -0500

    Update 10/19/17 16:05

--- hangman.js
+++ hangman.js
@@ -1,5 +1,8 @@
+var inquirer = require('inquirer');
+var numGuesses = 10;
+// Number of letters in the word to be guessed
+var numLetters = 0;
 var Word = require("./Word.js");
-
 var wordObject = new Word();
 
 var callback = function(word, definition, example) {
@@ -23,56 +26,91 @@ var callback = function(word, definition, example) {
  }
 
 
- var playGame = function(letters) {
- 	var inquirer = require('inquirer');
+ 
 
- 	
-	inquirer.prompt([
-	{
-		type: "input",
-		message: "Guess a letter! ",
-		name: "guess",
-		validate: function(value) {
-			if (value.match(/[A-Za-z]/)) {
-				return true;
-			}
-			else {
-				return "Please guess a letter";
+
+ var optionToPlayAgain = function() {
+ 	inquirer.prompt([
+ 		{
+      		type: "confirm",
+      		message: "Do you want to play again? ",
+      		name: "playAgain"
+    	}
+ 	]).then(function (answers) {
+
+ 	});
+ }
+
+
+ var playGame = function(letters) {
+ 	if (numGuesses > 0) {
+		inquirer.prompt([
+		{
+			type: "input",
+			message: "Guess a letter! ",
+			name: "guess",
+			validate: function(value) {
+				if (value.match(/[A-Za-z]/)) {
+					return true;
+				}
+				else {
+					return "Please guess a letter";
+				}
 			}
 		}
-	}
-	]).then(function (answers) {
-    	// answers.guess = answers.guess.toUpperCase();
-    	var containsGuess = false;
-    	for (var i = 0; i < letters.length; i++) {
-    		if (letters[i].value === answers.guess.toUpperCase()) {
-    			letters[i].display = letters[i].value;
-    			containsGuess = true;
-    		}
-    	}
-    	
-    	updateDisplay(containsGuess, letters);
-	});
- 	
+		]).then(function (answers) {
+			var containsGuess = false;
+			var displayString;
+
+			for (var i = 0; i < letters.length; i++) {
+				if (letters[i].value === answers.guess.toUpperCase()) {
+					letters[i].display = letters[i].value;
+					containsGuess = true;
+				}
+			}
+
+			if (!containsGuess) {
+				numGuesses--;
+			}
+
+			displayString = updateDisplay(containsGuess, letters);
+			if (!displayString.match(/_/)) {
+				console.log("You won!");
+				optionToPlayAgain();
+			}
+			else {
+				playGame(letters);
+			}	
+		});
+ 	}
+ 	else {
+ 		console.log("Game Over!");
+ 		optionToPlayAgain();
+ 	}
  }
 
 
  var updateDisplay = function(containsGuess, letters) {
- 	var displayString = "";
+ 	var displayString = ""; 
 
  	if (containsGuess) {
- 		console.log("CORRECT!");
+ 		console.log("\nCORRECT!\n");
  	}
  	else {
- 		console.log("INCORRECT :'\(");
+ 		console.log("\nINCORRECT :'\(");
+ 		if (numGuesses !== 1) {
+ 			console.log(numGuesses + " guesses remaining\n");
+ 		}
+ 		else {
+ 			console.log(numGuesses + " guess remaining\n");
+ 		}
  	}
 
  	for (i in letters) {
  		displayString += letters[i].display + " ";
  	}
- 	console.log(displayString);
+ 	console.log(displayString + "\n");
+ 	return displayString;
  }
 
  wordObject.getWord(callback);
-
-
