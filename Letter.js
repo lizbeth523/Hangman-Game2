41bfeea96f2242fddd0508c6642698d5e9e9182c
Elizabeth Tom <elizabeth_tom@excite.com>
2017-10-20 16:17:46 -0500

    Completed

--- Letter.js
+++ Letter.js
@@ -1,11 +1,11 @@
 var Letter = function(value) {
 	this.value = value;
 
-	if (this.value.match(/\s/)) {
-		this.display = " ";
-	}
-	else if (this.value.match(/^[A-Za-z]/)) {
+	if (this.value.match(/^[A-Za-z]/)) {
 		this.display = "_";
+	}
+	else if (this.value.match(/\s/)) {
+		this.display = " ";
 	}
 	else {
 		this.display = value;
