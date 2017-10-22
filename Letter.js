789ae5e27575d3ebf0b415330e4b01ee507443e5
Elizabeth Tom <elizabeth_tom@excite.com>
2017-10-22 18:29:04 -0500

    minor update to Letter.js

--- Letter.js
+++ Letter.js
@@ -1,7 +1,7 @@
 var Letter = function(value) {
 	this.value = value;
 
-	if (this.value.match(/^[A-Za-z]/)) {
+	if (this.value.match(/[A-Za-z]/)) {
 		this.display = "_";
 	}
 	else if (this.value.match(/\s/)) {
